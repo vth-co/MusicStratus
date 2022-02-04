c3bb8840902e32fe8506a7cc2364257522314e2d
Vu <vco.thien@gmail.com>
2022-02-03 16:57:55 -0800

    more tests

---
+++ backend/db/seeders/20220203213926-demo-user.js
@@ -0,0 +1,43 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkInsert('Users', [
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user1@user.io',
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'user2@user.io',
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password3')
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+      const Op = Sequelize.Op;
+      return queryInterface.bulkDelete('Users',{
+        username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+      }, {});
+  }
+};
