65ae2d25d1f0a9e03d9825be7aab09e34a445fca
Vu <vco.thien@gmail.com>
2023-09-12 19:49:37 -0700

    added max-width for splash screen

--- frontend/src/components/Pages/DiscoverPage/index.js
+++ frontend/src/components/Pages/DiscoverPage/index.js
@@ -173,40 +173,40 @@ const DiscoverPage = () => {
           </Carousel>
         </div>
           <div className="music-container">
-            {/* <AudioPlayer
+            <AudioPlayer
             className="audio-player"
             volume={0.2}
             layout="horizontal-reverse"
             src={currentTrack}
-            customAdditionalControls={[
-              <div>
-                <button>
-                  button 2<p className="song-title">{song?.title}</p>
-                </button>
-                <button>button 3 </button>
-                <button>button 4 </button>
-              </div>,
-            ]}
-            customProgressBarSection={[
-              RHAP_UI.CURRENT_TIME,
-              <div>/</div>,
-              RHAP_UI.DURATION,
-              RHAP_UI.PROGRESS_BAR,
-              RHAP_UI.VOLUME,
-            ]}
-            customVolumeControls={[]}
-            style={{
-              width: "600px",
-            }}
-            customAdditionalControls={[
-              <button>
-                <img className="image" src={song?.imageUrl} alt={""}></img>
-              </button>,
-            ]}
-          /> */}
-            <CustomAudioPlayer
+            // customAdditionalControls={[
+            //   <div>
+            //     <button>
+            //       button 2<p className="song-title">{song?.title}</p>
+            //     </button>
+            //     <button>button 3 </button>
+            //     <button>button 4 </button>
+            //   </div>,
+            // ]}
+            // customProgressBarSection={[
+            //   RHAP_UI.CURRENT_TIME,
+            //   <div>/</div>,
+            //   RHAP_UI.DURATION,
+            //   RHAP_UI.PROGRESS_BAR,
+            //   RHAP_UI.VOLUME,
+            // ]}
+            // customVolumeControls={[]}
+            // style={{
+            //   width: "600px",
+            // }}
+            // customAdditionalControls={[
+            //   <button>
+            //     <img className="image" src={song?.imageUrl} alt={""}></img>
+            //   </button>,
+            // ]}
+          />
+            {/* <CustomAudioPlayer
               currentTrack={currentTrack}
-            />
+            /> */}
           </div>
       </div>
     </>
