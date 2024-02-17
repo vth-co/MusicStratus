813733e6d271a4e2573b6a661680129fdeb14656
Vu <vco.thien@gmail.com>
2024-02-16 18:31:24 -0800

    adding edit and delete modal for songs but need work

--- frontend/src/components/CustomAudioPlayer/CardAudioPlayer.js
+++ frontend/src/components/CustomAudioPlayer/CardAudioPlayer.js
@@ -3,8 +3,27 @@ import "react-h5-audio-player/lib/styles.css";
 import "react-h5-audio-player/lib/styles.less"; //Use LESS
 import LikeButton from "../Likes/LikeButton";
 import "./CustomAudio.css"
+import { useSelector } from "react-redux";
+import EditSongModal from "../Songs/EditSongModal";
+import DeleteSongModal from "../Songs/DeleteSongModal";
 
 const CardAudioPlayer = ({ song }) => {
+  const sessionUser = useSelector((state) => state.session.user);
+  const userId = sessionUser.id;
+
+  let songEditButtons;
+  if (userId === song?.userId) {
+    songEditButtons = (
+      <div>
+        <EditSongModal />
+        <DeleteSongModal />
+      </div>
+    );
+  } else {
+    songEditButtons = (
+      <div></div>
+    )
+  }
 
   return (
     <>
@@ -23,13 +42,16 @@ const CardAudioPlayer = ({ song }) => {
         ]}
         customAdditionalControls={
             [
+<div className="song-edit-container">{songEditButtons}</div>,
 
             ]
           }
           customControlsSection={
             [
               RHAP_UI.MAIN_CONTROLS,
+              RHAP_UI.ADDITIONAL_CONTROLS,
               <LikeButton song={song} />,
+
             ]
           }
       />
